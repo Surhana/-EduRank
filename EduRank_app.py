ec0e535a0424d106a9aaf188267ed1ed3db198f5
Surhana <surhanaamani8@gmail.com>
2025-08-01 20:57:18 +0800

    Update EduRank_app.py

--- EduRank_app.py
+++ EduRank_app.py
@@ -112,8 +112,6 @@ else:
     st.subheader("Ranking the Chart")
     fig, ax = plt.subplots(figsize=(8,4))
     ax.bar(ranking['Alternative'], ranking['Performance Score'].astype(float), color='skyblue')
-    ax.set_xlabel("Alternatives")
-    ax.set_ylabel("Performance Score")
     plt.xticks(rotation=0)
     st.pyplot(fig)
 
